bae6ee5c61c8ae76616e942dfabd06c7d95d3ff1
bouzaien <mokhles.bouzaien@eurecom.fr>
2020-11-08 15:49:39 +0100

    add play script

---
+++ src/play.py
@@ -0,0 +1,26 @@
+import argparse
+
+from Battleship import Battleship
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--size", type=int, 
+        default=10, 
+        help="Field size"
+    )
+
+    parser.add_argument(
+        "--show_positions",
+        action="store_true",
+        help="Whether to show ships positions while playing",
+    )
+
+    args = parser.parse_args()
+
+    bs = Battleship(field_size=(args.size,args.size))
+    bs.play(show_positions=args.show_positions)
+
+if __name__ == "__main__":
+    main()
