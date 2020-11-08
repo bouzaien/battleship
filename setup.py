acd1a2896d773fbe8253e7633e4a1b50ba30b01d
bouzaien <mokhles.bouzaien@eurecom.fr>
2020-11-08 21:57:39 +0100

    package setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'battleship',
+    version="0.0.1",
+    author="Mokhles Bouzaien",
+    license="MIT",
+    packages = find_packages(),
+)
